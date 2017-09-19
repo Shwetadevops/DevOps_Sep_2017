51411601f647e6743939651b1250cbdaec335fdb
Surender Singh Rathore <surenbika@gmail.com>
2017-09-19 17:38:54 +0530

    Initial commit.

---
+++ src/test/java/org/tektutor/AppTest.java
@@ -0,0 +1,19 @@
+package org.tektutor;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+   
+{
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
